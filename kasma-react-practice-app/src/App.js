569d1488f68064d73c3b23297803340f2f3755c9
KasmaKeshvary <kasmakeshvary@gmail.com>
2024-04-13 13:58:19 +0330

    learn useState and array

--- kasma-react-practice-app/src/App.js
+++ kasma-react-practice-app/src/App.js
@@ -1,23 +1,36 @@
-// import logo from './images/logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  // let name = "Mahdi";
+  const [name , setName] = useState ("Mahdi");
+  const [events , setEvents] = useState ([
+    {title: "Thor: Love And Thunder (2022)", id: 1},
+    {title: "The Gray Man (2022)", id: 2},
+    {title: "The Sea Beast (2022)", id: 3},
+    {title: "Top Gun Maverick (2022)", id: 4},
+    {title: "Doctor Strange in Multiverse of Madness (2022)", id: 5},
+    {title: "Hustle (2022)", id: 6}
+  ]);
+
+  const handleClick = () => {
+    
+    setName("Kasma");
+    console.log(name);
+    // console.log(setName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src='logo.svg' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Kasma Practice
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <button onClick={handleClick}>Change Name</button>
+      {
+        events.map((event, index) => (
+          <div key={event.id}>
+            <p>{index + 1}-{event.title}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
