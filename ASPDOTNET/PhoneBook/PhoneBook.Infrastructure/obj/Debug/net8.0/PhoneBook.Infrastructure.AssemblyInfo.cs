a520f6d810a2ddb63b951c92842260cd19d03a5d
KasmaKeshvary <kasmakeshvary@gmail.com>
2025-06-23 12:30:56 +0330

    add needed package and Tests

--- ASPDOTNET/PhoneBook/PhoneBook.Infrastructure/obj/Debug/net8.0/PhoneBook.Infrastructure.AssemblyInfo.cs
+++ ASPDOTNET/PhoneBook/PhoneBook.Infrastructure/obj/Debug/net8.0/PhoneBook.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1fcc95a9f174a09f4be65795407d6ed7e714e45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e663ba276984577642e2dffa7bcb03e1d890065")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneBook.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
